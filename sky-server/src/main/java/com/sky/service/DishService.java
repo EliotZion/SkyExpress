b761df749a87a64f1c7427a899f6ae7a040077e5
zx125 <18117578361@163.com>
2026-02-02 19:42:22 +0000

    Day 3 菜品模块

---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,26 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface DishService {
+
+    /**
+     * 新增菜品以及口味
+     * @param dishDTO
+     */
+    public void saveWithFlavor(DishDTO dishDTO);
+
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    void deleteBatch(List<Long> ids);
+
+    DishVO getByIdWithFlavor(Long id);
+
+    void updateWithFlavor(DishDTO dishDTO);
+}
